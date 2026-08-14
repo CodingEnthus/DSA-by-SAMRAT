4e25eb9738ac0baf7395af85ffdbf610f12e6e6b
Samrat Dutta <151641833+CodingEnthus@users.noreply.github.com>
2026-08-14 12:08:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (61.09%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,7 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        if(n==0) return 0;
         if(n==1) return nums[0];
         if(n==2) return Math.max(nums[0],nums[1]);
         return Math.max(robLinear(0,n-2,nums),robLinear(1,n-1,nums));
