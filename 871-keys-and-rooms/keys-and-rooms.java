7b81bc78d2a3fd606da4471924b5274d59fbe92c
Samrat Dutta <151641833+CodingEnthus@users.noreply.github.com>
2026-08-31 11:30:47 +0530

    Time: 8 ms (5.04%) | Memory: 46.2 MB (64.09%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n=rooms.size();
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(new ArrayList<>());
+        }
+        int j=0;
+        for(int u=0;u<rooms.size();u++){
+            for(int v: rooms.get(u)) graph.get(u).add(v);
+        }
+        Queue<Integer> q=new LinkedList<>();
+        boolean[] visited=new boolean[n];
+        boolean[] keys=new boolean[n];
+        keys[0]=true;
+        q.offer(0);
+        while(!q.isEmpty()){
+            int p=q.poll();
+            visited[p]=true;
+            if(!keys[p]) return false;
+            for(int adj: graph.get(p)){
+                if(!visited[adj]|| !keys[adj]){
+                    keys[adj]=true;
+                    q.offer(adj);
+                }
+            }
+
+        }
+        for(boolean ans: visited){
+            if(!ans) return false;
+        }
+        return true;
+
+    }
+}
