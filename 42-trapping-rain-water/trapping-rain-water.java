bbb42d598188f25e5d1b289b64e7d0183aad76f1
Samrat Dutta <151641833+CodingEnthus@users.noreply.github.com>
2026-09-25 18:03:48 +0530

    Time: 1 ms (56.25%) | Memory: 49 MB (8.88%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,23 +1,30 @@
 class Solution {
     public int trap(int[] height) {
-        int lmax=0;
-        int rmax=0;
         int n=height.length;
-        int l=0;
-        int r=n-1;
+        int[] l=new int[n];
+        int[] r=new int[n];
         int ans=0;
-        while(l<r){
-            lmax=Math.max(lmax,height[l]);
-            rmax=Math.max(rmax,height[r]);
-            if(lmax<rmax){
-                ans+=lmax-height[l];
-                l++;
-            }else{
-                ans+=rmax-height[r];
-                r--;
-            }
+        l[0]=height[0];
+        r[n-1]=height[n-1];
+        for(int i=1;i<n;i++){
+            l[i]=Math.max(height[i],l[i-1]);
+        }
+        for(int i=n-2;i>=0;i--){
+            r[i]=Math.max(height[i],r[i+1]);
+        }
+        for(int i=0;i<n;i++){
+            ans+=Math.min(l[i],r[i])-height[i];
         }
         return ans;
-
+        
     }
 }
+
+//L-0 1 1 2 2 2 2 3 3 3 3 3 
+//R-3 3 3 3 3 3 3 3 2 2 2 1 
+//m -0 1 1 2 2 2 2 3 2 2 2 1
+// I-0 1 0 2 1 0 1 3 2 1 2 1 
+
+
+// ans-0 1  1+1+2+1+1
+// maxans=0 1
